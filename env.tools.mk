30ef2ea76479762c196a2c22a66e1f46d35f27cb
toshiki goto <gotoshi.euphonium@gmail.com>
2025-03-16 00:42:00 +0900

    add env.tools.mk

--- env.tools.mk
+++ env.tools.mk
@@ -1,1 +1,12 @@
+ifndef TOOLS_ENV
+TOOLS_ENV:=1
+
+include env.mk
+
 VERILATOR_VERSION:=5.032
+
+CONFIG_GENERATOR:=$(TOOLS_DIR)/config_generator/target/release/config_generator
+$(CONFIG_GENERATOR):
+	cd $(TOOLS_DIR)/config_generator && cargo build --release
+
+endif
