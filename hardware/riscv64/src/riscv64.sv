f80a2927aea62533446e7e63e758ab4d6d0268f2
toshiki goto <gotoshi.euphonium@gmail.com>
2025-03-16 01:14:28 +0900

    add top

---
+++ hardware/riscv64/src/riscv64.sv
@@ -0,0 +1,29 @@
+`include "arch/riscv64_params.svh"
+`include "fetch_types.svh"
+`include "decode_types.svh"
+
+module riscv64 (
+    input wire clk,
+    input wire rst_n
+);
+
+    fetch_types::input_t  fetch_input;
+    fetch_types::output_t fetch_output;
+    assign fetch_input.clk   = clk;
+    assign fetch_input.rst_n = rst_n;
+
+    fetch fetch_inst (
+        .fetch_input,
+        .fetch_output
+    );
+
+    decode_types::input_t  decode_input;
+    decode_types::output_t decode_output;
+    assign decode_input.instr = fetch_output.instr;
+
+    decode decode_inst (
+        .decode_input,
+        .decode_output
+    );
+
+endmodule
--- hardware/riscv64/test/src/tb_top.sv
+++ hardware/riscv64/test/src/tb_top.sv
@@ -1,31 +1,18 @@
 `include "tb_utils.svh"
 
 module tb_top;
-    logic expected;
+    logic clk;
+    logic rst_n;
+
+    riscv64 riscv64_inst (
+        .clk  (clk),
+        .rst_n(rst_n)
+    );
 
     initial begin
+        clk   = 0;
+        rst_n = 0;
         `TEST_START("tb_top.log");
-        #1;
-        expected = 1;
-        `TEST_EXPECTED(expected, 1, "1 == 1");
-        #1;
-        $display("expected = %0h", expected);
-        begin
-            `TEST_SECTION_START("section1");
-            expected = 1;
-            `TEST_EXPECTED(expected, 1, "1 == 1");
-            `TEST_EXPECTED(expected, 0, "1 == 0");
-            `TEST_SECTION_END();
-        end
-        begin
-            `TEST_SECTION_START("section2");
-            expected = 1;
-            `TEST_EXPECTED(expected, 1, "1 == 1");
-            `TEST_EXPECTED(expected, 0, "1 == 0");
-            `TEST_SECTION_END();
-        end
-        expected = 0;
-        `TEST_EXPECTED(expected, 1, "1 == 1");
 
         `TEST_RESULT();
     end
