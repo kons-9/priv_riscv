c880142ed0ee33faf63b5fbd3da61924e15d7459
toshiki goto <gotoshi.euphonium@gmail.com>
2025-03-16 01:52:28 +0900

    just fix top makefile

--- env.mk
+++ env.mk
@@ -1,9 +1,9 @@
 ifndef PROJECT_ENV
 PROJECT_ENV:=1
 
+PROJECT_ROOT := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 include $(PROJECT_ROOT)/env.tools.mk
 
-PROJECT_ROOT := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 TOOLS_DIR := $(PROJECT_ROOT)/tools
 HARDWARE_DIR := $(PROJECT_ROOT)/hardware
 SOFTWARE_DIR := $(PROJECT_ROOT)/software
