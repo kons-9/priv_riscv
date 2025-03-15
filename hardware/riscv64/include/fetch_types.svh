cedd89bc6df07c7994c8c345ef66e962214ffad9
toshiki goto <gotoshi.euphonium@gmail.com>
2025-03-16 02:52:09 +0900

    add fetch implementation

--- hardware/riscv64/include/fetch_types.svh
+++ hardware/riscv64/include/fetch_types.svh
@@ -1,12 +1,12 @@
 `ifndef FETCH_TYPES_SVH
 `define FETCH_TYPES_SVH
 
+`include "riscv64_params.svh"
+
 package fetch_types;
-    typedef struct {
-        logic clk;
-        logic rst_n;
-    } input_t;
-    typedef struct {logic [63:0] instr;} output_t;
+    localparam int PcWidth = $clog2(riscv64_params::InstrWidth);
+    typedef struct {logic [PcWidth-1:0] pc;} input_t;
+    typedef struct {logic [riscv64_params::InstrWidth -1:0] instr;} output_t;
 endpackage
 
 `endif  // FETCH_TYPES_SVH
--- hardware/riscv64/src/fetch.sv
+++ hardware/riscv64/src/fetch.sv
@@ -4,5 +4,17 @@ module fetch (
     input  fetch_types::input_t  fetch_input,
     output fetch_types::output_t fetch_output
 );
-    assign fetch_output.instr = 64'hdeadbeefdeadbeef;
+    reg [fetch_types::PcWidth-1:0] pc;
+
+    // using harvard architecture
+    reg [riscv64_params::InstrWidth - 1:0] instr_ram[0:1023];
+
+    initial begin
+        $readmemh(riscv64_params::InstrRomFile, instr_ram);
+    end
+
+    // 64bit / 8bit(=1byte) = 8
+    // 8 = 2^3
+    assign fetch_output.instr = instr_ram[fetch_input.pc>>3];
+
 endmodule
